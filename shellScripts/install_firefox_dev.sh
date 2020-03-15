2bf327dff61631947da88ede27f1bf5ad009b6c4
Mikha Davids <31388146+MikhaD@users.noreply.github.com>
2020-03-15 15:59:32 +0200

    Fixed .desktop location & added feedback

--- shellScripts/install_firefox_dev.sh
+++ shellScripts/install_firefox_dev.sh
@@ -1,33 +1,46 @@
 #!/usr/bin/env bash
 
 ffVersion="75.0b3"
-file="firefox_dev.desktop"
+file=${1:-firefox_dev}
 
 #Remove regular firefox to avoid conflicts
-sudo apt remove firefox
+sudo apt remove firefox > /dev/null
 #Navigate to /opt dir where firefox will be installed
 cd /opt
 #Download tar archive
-wget https://download-installer.cdn.mozilla.net/pub/devedition/releases/${ffVersion}/linux-x86_64/en-US/firefox-${ffVersion}.tar.bz2
+echo "Downloading Firefox Dev Edition..."
+wget https://download-installer.cdn.mozilla.net/pub/devedition/releases/${ffVersion}/linux-x86_64/en-US/firefox-${ffVersion}.tar.bz2 > /dev/null
 #Extract tar archive
-tar xvf firefox-${ffVersion}.tar.bz2
+echo "Extracting..."
+tar xvf firefox-${ffVersion}.tar.bz2 > /dev/null
 #Delete tar archive
-rm firefox-${ffVersion}.tar.bz2
+rm firefox-${ffVersion}.tar.bz2 > /dev/null
 #Change name of extracted dir from firefox to firefox_dev
-mv firefox firefox_dev
+mv firefox $file > /dev/null
 #Create link so user can run firefox
-ln -s /opt/firefox_dev/firefox /usr/local/bin/firefox_dev
+ln -s /opt/${file}/firefox /usr/local/bin/$file > /dev/null
 #Create .desktop file in order to create a desktop shortcut
-touch $file
+echo "Creating desktop icon..."
+touch /usr/share/applications/${file}.desktop
 echo "[Desktop Entry]
 Name=Firefox Dev Edition
 GenericName=Firefox Developer Edition
-Exec=/usr/local/bin/firefox_dev %u
+Exec=/usr/local/bin/${file} %u
 Terminal=false
-Icon=/opt/firefox_dev/browser/chrome/icons/default/default128.png
+Icon=/opt/${file}/browser/chrome/icons/default/default128.png
 Type=Application
 Categories=Application;Network;X-Developer;
 Comment=Firefox Developer Edition Web Browser
-StartupWMClass=Firefox Developer Edition" > $file
+StartupWMClass=Firefox Developer Edition" > /usr/share/applications/${file}.desktop
 #Give all groups permission to execute the shortcut
-chmod +x /usr/share/applications/$file
+chmod +x /usr/share/applications/${file}.desktop
+#Add firefox dev edition to favorites
+echo "Adding to favorites..."
+gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), '${file}.desktop']"
+#Navigate back to home directory
+cd ~
+
+echo "Firefox Developer edition installation complete"
+
+
+
